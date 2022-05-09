2b457338a4197038ff253a965cf687c477942cb0
Julien <moineau.julien@hotmail.fr>
2022-05-09 16:26:31 +0200

    7.Exploitez les méthodes communes aux objets - Essai 2 (syntaxe commune pour les getters et setters)

--- index.php
+++ index.php
@@ -9,7 +9,7 @@ class Encounter
 
     public static function probabilityAgainst(Player $playerOne, Player $playerTwo): float
     {
-        return 1/(1+(10 ** (($playerTwo->level - $playerOne->level)/400)));
+        return 1/(1+(10 ** (($playerTwo->getLevel() - $playerOne->getLevel())/400)));
     }
 
     public static function setNewLevel(Player $playerOne, Player $playerTwo, int $playerOneResult): void
@@ -18,24 +18,27 @@ class Encounter
             trigger_error(sprintf('Invalid result. Expected %s',implode(' or ', self::RESULT_POSSIBILITIES)));
         }
 
-        $playerOne->level += round(32 * ($playerOneResult - self::probabilityAgainst($playerOne, $playerTwo)));
+        $playerOne->setLevel($playerOne->getLevel() + round(32 * ($playerOneResult - self::probabilityAgainst($playerOne, $playerTwo)))) ;
     }
 }
 
 class Player
 {
-    public function __construct(private int $level)
+    private int $level;
+    
+    public function __construct(int $level)
     {
+        $this->level = $level;
     }
 
-    public function __get(string $name): int
+    public function getLevel(): int
     {
-        return $this->{$name};
+        return $this->level;
     }
 
-    public function __set(string $name, int $level)
+    public function setLevel(int $level)
     {
-        $this->{$name} = $level;
+        $this->level = $level;
     }
 }
 
@@ -53,8 +56,8 @@ Encounter::setNewLevel($jade, $greg, Encounter::RESULT_LOSER);
 
 echo sprintf(
     'les niveaux des joueurs ont évolués vers %s pour Greg et %s pour Jade',
-    $greg->level,
-    $jade->level
+    $greg->getLevel(),
+    $jade->getLevel()
 );
 
 exit(0);
